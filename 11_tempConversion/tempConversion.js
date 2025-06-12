0ea7b0f385b4aa01cb0f0e2dbc3f3bb7afc2fc83
dooory <78720364+dooory@users.noreply.github.com>
2025-06-12 16:17:34 +1200

    solved 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const roundNumber = function (number) {
+	return Math.round(number * 10) / 10
+}
+
+const convertToCelsius = function(fahrenheit) {
+	const converted = (fahrenheit - 32) * 5/9
+
+	return Number.isInteger(converted) ? converted : roundNumber(converted);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+	const converted = (celsius * 9/5) + 32
+
+	return Number.isInteger(converted) ? converted : roundNumber(converted);
 };
 
 // Do not edit below this line
