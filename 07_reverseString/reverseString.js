c7be98feac3c721e64989deae35340085fbe0c94
dooory <78720364+dooory@users.noreply.github.com>
2025-06-12 13:41:39 +1200

    kinda solved 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+	const stringArray = string.split("");	
+	let reversedString = "";
 
+	for (let index = stringArray.length - 1; index >= 0; index--) {	
+		const value = stringArray[index];
+
+		reversedString += value;
+	}
+
+	return reversedString;
 };
 
 // Do not edit below this line
