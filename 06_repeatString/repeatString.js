730b98b77649247447cbe4e6726e7f0974248bd7
dooory <78720364+dooory@users.noreply.github.com>
2025-06-12 13:16:33 +1200

    solved 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+	if (num === 0) {
+		return "";
+	} else if (num < 0) {
+		return "ERROR";
+	}
 
+	let repeatedString = "";
+
+	for (let index = 0; index < num-1; index++) {
+		repeatedString += string;
+	}
+
+	return repeatedString;
 };
 
 // Do not edit below this line
