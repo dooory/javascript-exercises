2c386e9836aecaa7e7392ef4d01efe17d374e184
dooory <78720364+dooory@users.noreply.github.com>
2025-06-12 14:47:00 +1200

    solved 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(intX, intY) {
+    console.log(Number.isInteger(intX))
 
+    if (
+        !Number.isInteger(intX) || 
+        !Number.isInteger(intY) ||
+        intX < 0 ||
+         intY < 0
+    ) return "ERROR";
+
+    const difference = Math.abs(intY - intX);
+
+    let sum = 0;
+    let maxInt = (intY > intX ? intX : intY) 
+
+    for (index = 0 + maxInt; index <= difference + maxInt; index++) {
+        sum += index;
+    }
+
+    return sum
 };
 
 // Do not edit below this line
