88890f7d1ce3b64fc04d2033abf6fa05bce283be
doory <78720364+dooory@users.noreply.github.com>
2025-06-30 15:37:18 +1200

    solved exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,23 @@
-const add = function() {
-	
-};
+const add = (x, y) => x + y;
 
-const subtract = function() {
-	
-};
+const subtract = (x, y) => x - y;
 
-const sum = function() {
-	
-};
+const sum = x => x.reduce(function(previous, current) {
+		return previous + current;
+	}, 0);
 
-const multiply = function() {
+const multiply = (x, y) => x.reduce((previous, current) => previous * current, 1);
 
-};
+const power = (x, y) => x ** y;
 
-const power = function() {
-	
-};
+const factorial = x => {
+	let factor = 1;
 
-const factorial = function() {
-	
+	for (index = 1; index <= x; index++) {
+		factor *= index;
+	}
+
+	return factor;	
 };
 
 // Do not edit below this line
