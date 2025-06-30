56dae1413d603abc402c25ebfedc39ed83e6e3bd
doory <78720364+dooory@users.noreply.github.com>
2025-06-30 16:57:22 +1200

    solved exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
+// 1, 1, 2, 3, 5, 8
+const fibonacci = function(numberIndex) {
+	if (numberIndex < 0) {
+		return "OOPS";
+	}
 
+	let sequence = [];
+
+	for (index = 0; index <= numberIndex; index++) {
+		if (sequence.length === 0) {
+			sequence.push(0);
+		} else if (sequence.length === 1) {
+			sequence.push(1);
+		} else {
+			console.log(index);
+			sequence.push(sequence[index - 1] + sequence[index - 2]);
+		};
+	};	
+
+	return sequence[numberIndex];
 };
+
+console.log(fibonacci("0"));
 
 // Do not edit below this line
 module.exports = fibonacci;
