d6dedf5b18418dfa26f452dc3cb49774469c1064
doory <78720364+dooory@users.noreply.github.com>
2025-06-30 16:14:14 +1200

    solved exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+	// Removes special characters and lower cases upper cased letter
+	let cleanString = string
+		.split("")
+		.reduce((previous, current) => {
+			if (current.match(/[A-z]|[1-9]/)) {
+				return previous + current.toLowerCase();
+			}
 
+			return previous;
+		}, "");
+
+	let reversedString = cleanString.split("").reverse().join("");
+
+	return reversedString == cleanString;
 };
 
 // Do not edit below this line
